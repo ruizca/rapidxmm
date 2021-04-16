f1a1785d9d6902dc53585637bc84a1acca9a49e5
Angel Ruiz <ruizca@noa.gr>
2021-04-16 16:08:54 +0300

    python api for XSA upper limit database

---
+++ rapidxmm.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+API for querying the XMM-Newton Upper Limit Database using the XSA interface
+
+@author: ruizca
+"""
+import json
+from time import time
+
+import numpy as np
+import requests
+from astropy.coordinates import ICRS
+from astropy.table import Table
+from astropy_healpix import HEALPix
+
+from pandas import DataFrame as df
+
+
+def _check_kwargs(obstype, instrum):
+    if obstype not in ["slew", "pointed", None]:
+        raise ValueError(f"Unknown obstype: {obstype}")
+        
+    if instrum not in ["PN", "M1", "M2", None]:
+        raise ValueError(f"Unknown instrum: {instrum}")
+
+
+def _filter_uls(uls, obstype, instrum):
+    if obstype:
+        mask = uls["obstype"] == obstype
+        uls = uls[mask]
+        
+    if instrum:
+        mask = uls["instrum"] == instrum
+        uls = uls[mask]
+        
+    return uls
+
+
+def _get_level(obstype):
+    if obstype == "pointed":
+        return 16
+    else:
+        return 15
+
+
+def _npixels_to_coords(npixels, level):
+    hp = HEALPix(nside=2**level, order="nested", frame=ICRS())
+    return hp.healpix_to_skycoord(npixels)
+
+
+def _query_xsa_uls(payload, obstype=None, instrum=None):
+    _check_kwargs(obstype, instrum)
+
+    r = requests.get(
+        "http://nxsa.esac.esa.int/nxsa-sl/servlet/get-uls", params=payload
+    )
+    r.raise_for_status()
+    
+    uls = Table.from_pandas(df.from_records(r.json()))
+    if uls:
+        uls = _filter_uls(uls, obstype, instrum)
+
+    return uls
+
+
+def query_radec(ra, dec, **kwargs):
+    if len(ra) != len(dec):
+        raise ValueError("ra and dec lengths don't match!")
+
+    payload = {
+        "ra": ";".join(str(round(c, 10)) for c in ra),
+        "dec": ";".join(str(round(c, 10)) for c in dec),
+    }
+    
+    return _query_xsa_uls(payload, **kwargs)
+    
+    
+def query_coords(coords, **kwargs):   
+    payload = {
+        "ra": ";".join(str(round(c.ra.deg, 10)) for c in coords),
+        "dec": ";".join(str(round(c.dec.deg, 10)) for c in coords),
+    }
+    
+    return _query_xsa_uls(payload, **kwargs)
+
+
+def query_npixels(npixels, obstype="pointed", instrum=None):
+    level = _get_level(obstype)
+    coords = _npixels_to_coords(npixels, level)
+    
+    return query_coords(coords, obstype=obstype, instrum=instrum)
